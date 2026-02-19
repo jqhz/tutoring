809f599e9d150fa51b68efd61e9c8ddacb1a67ac
jahzz <jasonhuang3030@gmail.com>
2026-02-19 00:12:18 -0600

    15:56 with 1 bug

---
+++ React/to-do-practice/src/components/task.tsx
@@ -0,0 +1,22 @@
+interface TaskItem {
+    id: number;
+    text: string;
+}
+
+type Props = {
+    task: TaskItem;
+    onDelete: (id:number) => void;
+}
+
+const Task = ({task, onDelete}: Props) => {
+    return (
+        <div>
+            <p>{task.text}</p>
+            <button onClick={() => onDelete(task.id)}>
+                Delete
+            </button>
+        </div>
+    )
+}
+
+export default Task;
